fbee8a8f706c9dcb86000b764592f2b8e16875b9
richardgagliano-source <richard.gagliano@gmail.com>
2026-02-11 15:38:14 -0800

    Update app.js

--- app.js
+++ app.js
@@ -290,7 +290,7 @@ tParams.set("limit", "200");
 }
 
 async function createTemplate(userId, name, split_type) {
-  const body = [{ user_id: userId, name, split_type }];
+const body = [{ name, split_type }];
   const created = await fetchJSON(`/rest/v1/workout_templates`, { method: "POST", body });
   return created?.[0];
 }
