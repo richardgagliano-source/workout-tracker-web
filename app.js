d1c6710e7f30b0263af2fde360547ed93ed7e03e
richardgagliano-source <richard.gagliano@gmail.com>
2026-02-16 16:40:27 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("APP VERSION: 2026-02-15-A");
+console.log("APP VERSION: 2026-02-16-A");
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 // --- Supabase config (your project) ---
